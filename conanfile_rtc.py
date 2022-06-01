100ff5546c10ceb9176993ffe22c626d846c1734
Chris Warner <cwarner@esri.com>
2022-06-01 15:48:00 -0700

    build: add conanfile

---
+++ conanfile_rtc.py
@@ -0,0 +1,21 @@
+from conans import ConanFile
+
+
+class StdUUIDConan(ConanFile):
+    name = "stduuid"
+    version = "1.2.2"
+    url = "https://github.com/Esri/stduuid"
+    license = "https://github.com/Esri/stduuid/blob/master/LICENSE"
+    description = "A C++17 cross-platform single-header library implementation for universally unique identifiers"
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/stduuid/"
+
+        # headers
+        self.copy("uuid.h", src=base + "include", dst=relative + "stduuid")
+
+        # no libraries; header-only
